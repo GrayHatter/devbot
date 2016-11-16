bd734728e16212fe729b62c94b423b1c30a21d57
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2016-11-16 13:43:19 -0800

    the commit with basic milestone support

--- devbot.hs
+++ devbot.hs
@@ -7,7 +7,13 @@ import              Data.List
 import qualified    Data.Vector as V
 import              Data.Maybe
 import qualified    Data.Text as P
+import qualified    Data.ByteString.Char8 as CHAR8
+import qualified    Data.ByteString.Lazy.Char8 as LCHAR8
 import              Network
+import              Network.HTTP.Client
+import              Network.HTTP.Client.TLS
+import              Network.HTTP.Types.Status (statusCode)
+import              Data.Aeson (object, (.=), encode)
 import              System.Exit
 import              System.IO
 import              Text.Printf
@@ -46,6 +52,8 @@ data User = User
     ,   host :: String
     }
 
+-- urlSortener :: TODO
+
 main :: IO ()
 main = bracket connect disconnect loop
   where
@@ -54,7 +62,7 @@ main = bracket connect disconnect loop
 
 connect :: IO Bot
 connect = notify $ do
-    irc_conn <- connectTo server $ PortNumber $ fromIntegral port
+    irc_conn <- connectTo server $ PortNumber $ fromIntegral Main.port
     hSetBuffering irc_conn NoBuffering
     return (Bot irc_conn)
   where
@@ -97,6 +105,11 @@ eval :: String -> String -> String -> Net ()
 eval sender target "!die" = do
     privMsg target "Sure, I'll just DIE then!"
     write "QUIT" ":My death was ordered" >> io (exitWith ExitSuccess)
+eval sender target "!m" = do
+    text <- io (nextMileStone "TokTok" "Maybe target milestone")
+    privMsg target text
+eval sender target "!status iphy" = do
+    privMsg target "iphy's current status :: https://img.shields.io/badge/iphy-savage-red.svg"
 eval sender target msg
     | "!echo " `isPrefixOf` msg = privMsg target $ drop 6 msg
     | msg =~ regex = do
@@ -109,10 +122,40 @@ eval sender target msg
 privMsg :: String -> String -> Net ()
 privMsg to text = write "PRIVMSG" $ to ++ " :" ++ text
 
+githubToIRC :: String -> String
+githubToIRC x = x
+
+githubShort :: String -> String -> String -> IO (Maybe String)
+githubShort url pre str = do
+    manager <- newManager tlsManagerSettings
+    let requestText = [  ("url", CHAR8.pack url)
+                      ,  ("code", CHAR8.pack (pre ++ str))
+                      ]
+    initRequest <- parseRequest "https://git.io"
+    let request = urlEncodedBody requestText $ initRequest { method = "POST" }
+    response <- httpLbs request manager
+    putStrLn $ LCHAR8.unpack (responseBody response)
+    putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
+
+    let headers = responseHeaders response
+    let res = (lookup "location" headers)
+    if isJust res
+        then return (Just $ CHAR8.unpack $ fromJust res)
+        else return (Nothing)
+
+
+nextMileStone :: String -> String -> IO (String)
+nextMileStone pre _ = do
+    -- get from github
+    str <- githubShort "https://github.com/TokTok/c-toxcore/milestone/11" "TokTok" "-v0.0.5"
+    if isJust str
+        then return $ fromJust str
+        else return "Error getting milestone... Sorry about that"
+
 parseAssigned :: G.Issue -> IO [String]
 parseAssigned issue = do
     let assigned = V.toList $ G.issueAssignees issue
-    let names = map (P.unpack . G.untagName . G.simpleUserLogin) assigned
+    let names = map (githubToIRC . P.unpack . G.untagName . G.simpleUserLogin) assigned
     return (names)
 
 checkIssue :: String -> IO (Maybe String)
@@ -125,9 +168,10 @@ checkIssue msg = do
         Left  err -> return (Nothing)
         Right real_issue -> do
             users <- parseAssigned real_issue
-            let user = (intercalate " " users)
-            let url  = (P.unpack . G.getUrl $ fromJust $ G.issueHtmlUrl real_issue)
-            let str  = url ++ " Assigned to: " ++ user
+            let user  = (intercalate " " users)
+            let url   = (P.unpack . G.getUrl $ fromJust $ G.issueHtmlUrl real_issue)
+            let title = (P.unpack $ G.issueTitle real_issue)
+            let str  = url ++ " " ++ title ++ " (Assigned to: " ++ user ++ ")"
             return (Just str)
 
 io :: IO a -> Net a
